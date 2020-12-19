90eee0c4a7d6d65e26776af3222938f947243c0b
Victor Douglas Rumanski <50980148+victorrumanski@users.noreply.github.com>
2020-12-19 06:49:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Author } from "./features/posts/Author";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Author />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/features/posts/Author.js
@@ -0,0 +1,5 @@
+import { React } from "react";
+
+export const Author = () => {
+  return <h2>MY pretty go component</h2>;
+};
